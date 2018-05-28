5d6642dce4c814b0fc350964978b654be33e848b
Arjun Maini <arjunmaini007@gmail.com>
2018-05-28 23:06:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
-// Connect to mongo
 mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     if(err){
         throw err;
@@ -13,27 +12,23 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     client.on('connection', function(socket){
         let chat = db.collection('chats');
 
-        // Create function to send status
         sendStatus = function(s){
             socket.emit('status', s);
         }
 
-        // Get chats from mongo collection
+        // Get chats from mongo 
         chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
             if(err){
                 throw err;
             }
 
-            // Emit the messages
             socket.emit('output', res);
         });
 
-        // Handle input events
         socket.on('input', function(data){
             let name = data.name;
             let message = data.message;
 
-            // Check for name and message
             if(name == '' || message == ''){
                 // Send error status
                 sendStatus('Please enter a name and message');
@@ -51,7 +46,6 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
             }
         });
 
-        // Handle clear
         socket.on('clear', function(data){
             // Remove all chats from collection
             chat.remove({}, function(){
